0e2ffba203794a82456a9d63a2c02335c49a11dd
LySeney <2283539919@qq.com>
2019-09-30 13:50:24 +0800

    add build script

--- app/src/main/java/com/example/hockeyappdemo/MainActivity.java
+++ app/src/main/java/com/example/hockeyappdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hockeyappdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,11 +14,20 @@ import com.microsoft.appcenter.crashes.Crashes;
 import com.microsoft.appcenter.crashes.CrashesListener;
 import com.microsoft.appcenter.crashes.ingestion.models.ErrorAttachmentLog;
 import com.microsoft.appcenter.crashes.model.ErrorReport;
+import com.microsoft.appcenter.distribute.Distribute;
+import com.microsoft.appcenter.distribute.DistributeListener;
+import com.microsoft.appcenter.distribute.ReleaseDetails;
 
 import net.hockeyapp.android.CrashManager;
 import net.hockeyapp.android.CrashManagerListener;
+import net.hockeyapp.android.ExceptionHandler;
 import net.hockeyapp.android.Tracking;
+import net.hockeyapp.android.UpdateManager;
+import net.hockeyapp.android.UpdateManagerListener;
 import net.hockeyapp.android.metrics.MetricsManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -31,23 +41,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initHockeyApp();
-        initAppcenter();
+        //initAppcenter();
     }
 
 
     private void initHockeyApp(){
-        CrashManager.register(this, "e2c15e61b7f3431189155a1a7fb955f5", new CrashManagerListener() {
+
+        UpdateManager.register(this, "6f258871556141b2bc60cec4ef5efaac", new UpdateManagerListener() {
+            @Override
+            public void onUpdateAvailable(JSONArray data, String url) {
+                super.onUpdateAvailable(data, url);
+                for (int i = 0; i < data.length(); i++) {
+                    try {
+                        Log.e("item: ",data.getJSONObject(i).toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        CrashManager.register(this, "6f258871556141b2bc60cec4ef5efaac", new CrashManagerListener() {
             @Override
             public boolean shouldAutoUploadCrashes() {
                 return true;
             }
         });
 
+
+
+    }
+
+    public void click3(View view){
+        ExceptionHandler.saveException(new RuntimeException("ExceptionHandler For Test"), Thread.currentThread(), new CrashManagerListener() {
+            @Override
+            public boolean shouldAutoUploadCrashes() {
+                return true;
+            }
+
+            @Override
+            public void onCrashesSent() {
+                super.onCrashesSent();
+                Log.e("ExceptionHandler","自定义crash 发送");
+            }
+        });
     }
 
     private void initAppcenter(){
         AppCenter.setLogLevel(Log.VERBOSE);
-        AppCenter.start(getApplication(),"839abbd6-ee35-4d0a-8635-62ff81fb4fd6", Crashes.class, Analytics.class);
+        Distribute.setEnabled(true);
+        AppCenter.start(getApplication(),"6f258871-5561-41b2-bc60-cec4ef5efaac", Crashes.class, Analytics.class);//Distribute.class
         Analytics.trackEvent("SDK INIT");
         AppCenterTracking.startUsage(this);
     }
@@ -60,7 +103,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void click2(View view){
-
         Intent intent = new Intent(this,Main2Activity.class);
 
         //startActivityForResult(intent,0);
