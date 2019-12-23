2f80352f24599e4f66b176aa3971b1750a9201d8
LySeney <2283539919@qq.com>
2019-12-23 15:51:38 +0800

    js update 4

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -19,7 +19,7 @@ npm i moment -S
 npm i request -S
 
 
-node script.txt -a 18434391078-163.com/HockeyApp1 -t e687aef8e656a7a0c98e3d8d5be86fe0ce17bb35 -v 1 --sep #
+node script.txt -a 18434391078-163.com/HockeyApp1 -t e687aef8e656a7a0c98e3d8d5be86fe0ce17bb35 -v 1 
 
 echo "#####################################################"
 
